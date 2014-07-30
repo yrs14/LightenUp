422346d0f1202f46991ab49b5006112942541dde
Clovis Parker-Jervis <cpj234@gmail.com>
2014-07-30 11:49:33 +0100

    flask

--- server-side.py
+++ server-side.py
@@ -1,6 +1,5 @@
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask,request,render_template,url_for
+
 app = Flask(__name__)
 
 def calcBMI(height,weight):
@@ -10,14 +9,15 @@ def calcBMI(height,weight):
     #    BMI = BMI * 703
     return(BMI)
 
-@app.route('/')
-def index():
-    f=open("questions.html","r+")
-    return f.read()
+#@app.route('/')
+#def index():
+#    f=open("questions.html","r+")
+#    return f.read()
 #use below for a template
-#@app.route('/resultspage.html')
+
+@app.route('/')
 def resultstemplate():
-    return render_template('resultspage.html', name=None)
+    return render_template('questions.html', string='header', theme_static='/static')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
